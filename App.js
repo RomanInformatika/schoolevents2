4432ca24a8ffd5871b73d58989ba05b6f9ffa484
RomanInformatika <officepm@yandex.ru>
2022-10-17 23:02:13 +0300

    first commin

---
+++ App.js
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+export default function App() {
+  let [isLoading, setIsLoading] = useState(true);
+  let [error, setError] = useState();
+  let [response, setResponse] = useState();
+
+  useEffect(() => {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          setResponse(result);
+          setIsLoading(false);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+        }
+      )
+  }, []);
+
+  const getContent = () => {
+    if (isLoading) {
+      return <ActivityIndicator size="large" />;
+    }
+
+    if (error) {
+      return <Text>{error}</Text>
+    }
+     return <Text>Bitcoin (USD): {response["bpi"]["USD"].rate}</Text>;
+  };
+
+  return (
+    <View style={styles.container}>
+      {getContent()}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
