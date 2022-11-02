a400a0bb66c27c0019526cb4d66592b5ecbbc0ef
RomanInformatika <officepm@yandex.ru>
2022-11-03 00:08:33 +0300

    flatlist

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
 
 export default function App() {
   let [isLoading, setIsLoading] = useState(true);
@@ -8,11 +8,16 @@ export default function App() {
   let [response, setResponse] = useState();
 
   useEffect(() => {
-    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    fetch("https://school1298.ru/cl/calendar.json",
+      // fetch("https://api.coindesk.com/v1/bpi/currentprice.json",
+      {
+        method: 'GET',
+        headers: { 'Content-Type': 'text/plain' }
+      })
       .then(res => res.json())
       .then(
         (result) => {
-          console.log(result)
+          console.log(result.value)
           setResponse(result);
           setIsLoading(false);
         },
@@ -31,12 +36,34 @@ export default function App() {
     if (error) {
       return <Text>{error}</Text>
     }
-     return <Text>Bitcoin (USD): {response["bpi"]["USD"].rate}</Text>;
+    return <Text>Bitcoin (USD): {response["bpi"]["USD"].rate}</Text>;
   };
 
+  const data2 = [{ Address: 'tt' }, { Address: 'tt2' }]
+
   return (
-    <View style={styles.container}>
-      {getContent()}
+    // <View style={styles.container}>
+    <View >
+      {/* {getContent()} */}
+      {!isLoading && <Text>{response.value[0].Address}</Text>}
+      <FlatList
+        data={response?.value}
+        renderItem={(item) => {
+          return <>
+            <Text>'Что: {item.item.Group[0].Title}, '</Text>
+            <Text>'Где: {item.item.Address}, '</Text>
+            <Text>'Кто: {item.item.MainMan.Title}, '</Text>
+            <Text>'Дата: {item.item.DateStart}, '</Text>  
+            <Text>'Время: {item.item.Time}, '</Text>  
+
+            <Text>-------------</Text>
+          </>
+        }}
+        keyExtractor={(item) => item.id}
+        vertical
+      />
+
+
       <StatusBar style="auto" />
     </View>
   );
